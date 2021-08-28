71295ca197beeff40da3f65df44fa0056c919fef
Rodrigo Ueda <rodrigoyueda@gmail.com>
2021-08-28 01:31:24 -0300

    Keeping files from compiling at build

--- Editor/HierarchyCustomStyleSettings.cs
+++ Editor/HierarchyCustomStyleSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -72,3 +73,4 @@ namespace UnityBaseCode.Editor
         }
     }
 }
+#endif
--- Editor/HierarchyWindowGroupHeader.cs
+++ Editor/HierarchyWindowGroupHeader.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Text.RegularExpressions;
 using System;
@@ -69,3 +70,4 @@ namespace UnityBaseCode.Editor
         }
     }
 }
+#endif
